345eef86b5b014ff757df8aab21b729deea945c2
Valentin Clement <clementval@gmail.com>
2015-11-09 17:48:33 +0100

    Add loops

--- test/sample.f90
+++ test/sample.f90
@@ -1,5 +1,21 @@
+PROGRAM f2py_eval
+  CALL test
+END PROGRAM f2py_eval
+
 SUBROUTINE test
+  INTEGER :: i
+  INTEGER :: istart = 1
+  INTEGER :: iend = 10
   !$claw loop-fusion
+  DO i = istart, iend
+    print*,'body loop 1', i
+  END DO
+
+  !$claw loop-fusion
+  DO i = istart, iend
+    print*,'body loop 2', i
+  END DO
+
   !$claw loop-interchange
   !$claw looooop-fusion
   !$acc parallel
