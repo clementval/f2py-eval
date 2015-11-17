12cdf377644335889c1081c8d957969614a06805
Valentin Clement <clementval@gmail.com>
2015-11-17 09:15:31 +0100

    Wrap function into a class to hold some parsing information

--- parse_file.py
+++ parse_file.py
@@ -8,64 +8,62 @@ from fparser import readfortran
 from fparser import block_statements
 
 
-def print_block_info(block):
-   print '####### BLOCK INFORMATION #######'
-   print 'Root blocktype: ' + block.blocktype
-   print '  name:   ' + block.blocktype
-   print '  content:'
-   print block.content
+class claw_parser:
+    def __init__(self, infile, outfile, keep_pragma=True):
+        self.infile = infile
+        self.outfile = outfile
+        self.keep_pragma = keep_pragma
 
-def process_main_block(block):
-    for stmt in block.content:
-        process_stmt(stmt)
+    def __parse(self):
+        reader = api.get_reader(self.infile, True, False, None, None)
+        parser = parsefortran.FortranParser(reader, False)
+        parser.parse()
+        return parser.block
 
-def process_stmt(stmt):
-   process_item(stmt)
-   if hasattr(stmt, 'content'):
-       for s in stmt.content:
-           process_stmt(s)
-
-
-# Possible item types: Line, SyntaxErrorLine, Comment, MultiLine,
-# SyntaxErrorMultiLine
-def process_item(stmt):
-    if hasattr(stmt, 'item'):
-        if isinstance(stmt.item, readfortran.Comment):
-            process_comment(stmt.item)
-        elif isinstance(stmt.item, readfortran.Line):
-            process_line(stmt.item)
-
-def process_comment(comment):
-    if not isinstance(comment, readfortran.Comment):
-        return
-    print comment.comment
-
-def process_line(line):
-    if not isinstance(line, readfortran.Line):
-        return
-    print line.line
+    def translate(self):
+        main_block = self.__parse()
+        self.__process_main_block(main_block)
 
 
-def f2py_parse(inputfile, outputfile, analyze=False):
-   reader = api.get_reader(inputfile, True, False, None, None)
-   parser = parsefortran.FortranParser(reader, False)
-   parser.parse()
+    def print_block_info(block):
+        print '####### BLOCK INFORMATION #######'
+        print 'Root blocktype: ' + block.blocktype
+        print '  name:   ' + block.blocktype
+        print '  content:'
+        print block.content
 
-   #print_block_info(parser.block)
-   process_main_block(parser.block)
+    def __process_main_block(self, block):
+        for stmt in block.content:
+            self.__process_stmt(stmt)
 
-   if outputfile == '':
-      #print parser.block
-      outputfile = outputfile
-   else:
-      output = open(outputfile, 'w+')
-      output_str = str(parser.block)
-      output.write(output_str)
-   if analyze == True:
-      analyse_result = parser.analyze()
+    def __process_stmt(self, stmt):
+        self.__process_item(stmt)
+        if hasattr(stmt, 'content'):
+            for s in stmt.content:
+                self.__process_stmt(s)
+
+
+    # Possible item types: Line, SyntaxErrorLine, Comment, MultiLine,
+    # SyntaxErrorMultiLine
+    def __process_item(self, stmt):
+        if hasattr(stmt, 'item'):
+            if isinstance(stmt.item, readfortran.Comment):
+                self.__process_comment(stmt.item)
+            elif isinstance(stmt.item, readfortran.Line):
+                self.__process_line(stmt.item)
+
+    def __process_comment(self, comment):
+        if not isinstance(comment, readfortran.Comment):
+            return
+        print comment.comment
+
+    def __process_line(self, line):
+        if not isinstance(line, readfortran.Line):
+            return
+        print line.line
 
 def print_help():
-   print 'parse_file.py -i <inputfile> -o <outputfile> [-a (analyze while parsing)]'
+   print 'parse_file.py -i <inputfile> -o <outputfile>'
 
 def main(argv):
    inputfile = ''
@@ -84,13 +82,12 @@ def main(argv):
          inputfile = arg
       elif opt == "-o":
          outputfile = arg
-      elif opt in ("-a"):
-         analyze = True
    if inputfile == '':
       print_help()
       sys.exit()
    else:
-      f2py_parse(inputfile, outputfile, analyze)
+      claw = claw_parser(inputfile, outputfile)
+      claw.translate()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
