8edcefabc26c63c7d7fbdf28bdf91620d58df260
Valentin Clement <clementval@gmail.com>
2015-11-17 09:54:13 +0100

    Add test code with only valid pragma

---
+++ test/loop_fusion.f90
@@ -0,0 +1,23 @@
+PROGRAM f2py_eval
+  CALL test
+END PROGRAM f2py_eval
+
+SUBROUTINE test
+  INTEGER :: i
+  INTEGER :: istart = 1
+  INTEGER :: iend = 10
+  !$claw loop-fusion
+  DO i = istart, iend
+    print*,'body loop 1', i
+  END DO
+
+  !$claw loop-fusion
+  DO i = istart, iend
+    print*,'body loop 2', i
+  END DO
+
+  !$claw loop-fusion
+  DO i = istart, iend
+    print*,'body loop 2', i
+  END DO
+END SUBROUTINE test
