405d415a4006727d2c4c31237f234c61b318f042
Valentin Clement <clementval@gmail.com>
2015-11-24 12:20:11 +0100

    Add test with v2 for several loop fusion

---
+++ test/loop/fusion2/reference_v2.f90
@@ -0,0 +1,29 @@
+  ! Simple program to test the loop-fusion directive
+
+  PROGRAM loop_fusion
+    CALL clawloop
+  END PROGRAM loop_fusion
+
+  SUBROUTINE clawloop()
+    INTEGER i
+    !$claw loop-fusion group(g1)
+    DO i=1,5
+      PRINT *, 'First loop body:', i
+      PRINT *, 'Second loop body:', i
+    END DO 
+
+
+    !$claw loop-fusion group(g2)
+    DO i=1,5
+      PRINT *, 'Third loop body:', i
+      PRINT *, 'Fourth loop body:', i
+    END DO 
+
+
+    !$claw loop-fusion
+    DO i=1,5
+      PRINT *, 'Fifth loop body:', i
+      PRINT *, 'Sixth loop body:', i
+    END DO 
+
+  END SUBROUTINE clawloop
